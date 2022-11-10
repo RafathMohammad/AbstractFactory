9e0596eee0e802c2bb5c50cf9daee8b9338395e4
Mohammad <rm185527@ncr.com>
2022-11-10 11:12:35 +0530

    AbstractFactory Changes

---
+++ Factories/CompanyAsset.cs
@@ -0,0 +1,18 @@
+﻿using AbstractFactoryDesignpattern.Interfaces;
+using AbstractFactoryDesignpattern.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactoryDesignpattern.Factories
+{
+    abstract class CompanyAsset
+    {
+        public abstract ILaptop GetLaptop();
+        public abstract IDesktop GetDesktop();
+        public abstract IHeadset GetHeadset();
+
+    }
+}
